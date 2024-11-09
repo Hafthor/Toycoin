363fd30513c071bdfc92450602e414cc937e7484
Hafthor <git@hafthor.net>
2024-11-09 12:19:45 -0500

    including one transaction

--- Toycoin/Program.cs
+++ Toycoin/Program.cs
@@ -2,20 +2,22 @@
 using System.Diagnostics.Contracts;
 using System.Security.Cryptography;
 
-const ulong microReward = 1_000_000; // 1 coin
 const string walletFile = "wallet.dat", blockchainFile = "blockchain.txt";
 byte[] data;
 bool walletExists = File.Exists(walletFile);
 byte[] walletData = walletExists ? File.ReadAllBytes(walletFile) : null;
+byte[] walletPublicKey;
+Transaction tx;
 using (Wallet wallet = walletExists ? new(walletData) : new()) {
     if (!walletExists) {
         walletData = wallet.FileData;
         File.WriteAllBytes(walletFile, walletData);
     }
     Array.Clear(walletData);
-    Transaction tx = wallet.CreateTransaction(wallet.PublicKey, 0, 0), tx2 = new(data = tx.Buffer);
+    tx = wallet.CreateTransaction(walletPublicKey = wallet.PublicKey, 0, 0);
+    Transaction tx2 = new(data = tx.Buffer);
     Console.WriteLine("tx: " + Convert.ToHexString(data));
-    data = [.. data, .. wallet.PublicKey, ..  BitConverter.GetBytes(microReward)];
+    data = [.. data, .. wallet.PublicKey, ..  BitConverter.GetBytes(Transaction.MicroReward)];
 }
 
 Block block = null;
@@ -36,10 +38,10 @@ for (;;) { // mining loop
     //   receiver=our address, amount=reward+fees
     // amount is recorded as convenience and must be checked against current reward amount and sum of the fees.
     var sw = Stopwatch.GetTimestamp();
-    block = new Block(block, data);
-    var elapsed = Stopwatch.GetElapsedTime(sw);
-    Console.WriteLine("{0} {1:N0} {2:N3}s {3}Mhps", block, block.HashCount, elapsed.TotalSeconds,
-        block.HashCount / elapsed.TotalSeconds / 1E6);
+    block = new Block(block, tx == null ? [] : [tx], walletPublicKey);
+    tx = null;
+    var elapsed = Stopwatch.GetElapsedTime(sw).TotalSeconds;
+    Console.WriteLine("{0} {1:N0} {2:N3}s {3}Mhps", block, block.HashCount, elapsed, block.HashCount / elapsed / 1E6);
     File.AppendAllLines(blockchainFile, [block.FileString()]);
 }
 
@@ -47,6 +49,15 @@ public class Block {
     private readonly Block _previous;
     private readonly byte[] _nonce, _data, _hash;
     public readonly int HashCount;
+
+    public Block(Block previous, IList<Transaction> transactions, byte[] myPublicKey) : this(previous,
+        MakeData(transactions, myPublicKey)) {
+    }
+    
+    private static byte[] MakeData(IList<Transaction> transactions, byte[] myPublicKey) {
+        ulong reward = transactions.Aggregate(Transaction.MicroReward, (sum, tx) => sum + tx.MicroFee);
+        return [.. transactions.SelectMany(tx => tx.Buffer).ToArray(), .. myPublicKey, ..BitConverter.GetBytes(reward)];
+    }
 
     public Block(Block previous, byte[] data, byte[] nonce = null, byte[] hash = null) {
         _previous = previous;
@@ -80,9 +91,13 @@ public class Block {
 }
 
 public class Transaction {
+    public const ulong MicroReward = 1_000_000; // 1 coin
     private readonly byte[] _sender, _receiver;
     private readonly ulong _microAmount, _microFee;
     private readonly byte[] _signature;
+    
+    public ulong MicroAmount => _microAmount;
+    public ulong MicroFee => _microFee;
 
     public Transaction(byte[] sender, byte[] receiver, ulong microAmount, ulong microFee, byte[] privateKey) {
         Contract.Assert(sender.Length == 140 && receiver.Length == 140, "Invalid public key length");
