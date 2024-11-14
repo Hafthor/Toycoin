fcce7aeea6292a1efa9b9cc5538c6e226fece900
Hafthor <git@hafthor.net>
2024-11-14 11:37:17 -0500

    clean up extensions

--- Toycoin/Extensions.cs
+++ Toycoin/Extensions.cs
@@ -1,26 +1,12 @@
 namespace Toycoin;
 
 public static class Extensions {
-    public static byte[] Coalesce(this IList<byte[]> arrays) {
-        if (arrays.Count == 0) return [];
-        if (arrays.Count == 1) return arrays[0];
-        var result = new byte[arrays.Sum(a => a.Length)];
-        var offset = 0;
-        foreach (var a in arrays) {
-            Buffer.BlockCopy(a, 0, result, offset, a.Length);
-            offset += a.Length;
+    public static bool IsLessThan<T>(this ReadOnlySpan<T> first, ReadOnlySpan<T> second) where T : IComparable<T> {
+        for (int i = 0, length = Math.Min(first.Length, second.Length); i < length; i++) {
+            int c = first[i].CompareTo(second[i]);
+            if (c != 0)
+                return c < 0;
         }
-        return result;
-    }
-
-    public static bool IsLessThan(this byte[] first, byte[] second) => IsLessThan(first.AsSpan(), second.AsSpan());
-    public static bool IsLessThan(this ReadOnlySpan<byte> first, byte[] second) => IsLessThan(first, second.AsSpan());
-    public static bool IsLessThan(this byte[] first, ReadOnlySpan<byte> second) => IsLessThan(first.AsSpan(), second);
-
-    public static bool IsLessThan(this ReadOnlySpan<byte> first, ReadOnlySpan<byte> second) {
-        for (int i = 0, length = Math.Min(first.Length, second.Length); i < length; i++)
-            if (first[i] != second[i])
-                return first[i] < second[i];
         return true;
     }
 }
