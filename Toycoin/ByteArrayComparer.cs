6328344540307fdea017fbd8236e5985f016507e
Hafthor <git@hafthor.net>
2024-11-15 19:36:40 -0500

    gethashcode

--- Toycoin/ByteArrayComparer.cs
+++ Toycoin/ByteArrayComparer.cs
@@ -11,5 +11,5 @@ public class ByteArrayComparer : EqualityComparer<byte[]> {
             ? first == second
             : ReferenceEquals(first, second) || first.Length == second.Length && first.SequenceEqual(second);
 
-    public override int GetHashCode(byte[] obj) => obj.Length;
+    public override int GetHashCode(byte[] obj) => obj.Aggregate(0, (a, b) => a * 31 + b);
 }
